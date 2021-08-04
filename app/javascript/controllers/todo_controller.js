e35b51da65cf21deaf9c80ebc069e7705c442a02
asyraffff <amirulasyraf4.0x@gmail.com>
2021-08-04 09:52:04 +0800

    init

---
+++ app/javascript/controllers/todo_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = [ "completed" ]
+    static values = { updateUrl: String }
+
+    toggle(event) {
+        let formData = new FormData()
+        formData.append("todo[completed]", this.completedTarget.checked)
+    }
+
+    fetch(this.updateUrlValue, {
+      body: formData,
+      method: "PATCH",
+      credentials: "include",
+      dataType: "script",
+      headers: {
+        "X-CSRF-Token": token,
+      },
+    }).then(function (response) {
+      if (response.status != 204) {
+        event.target.checked = !event.target.checked;
+      }
+    });
+
+    const token = document.getElementsByName(
+      "csrf-token"
+    )[0].content;
+
+    // getMetaValue("csrf-token")
+
+    // function getMetaValue(name) {
+    //   const element = document.head.querySelector(`meta[name="${name}"]`);
+    //   return element.getAttribute("content");
+    // }
+}
